956509efb1c89127681e403b2ff2e56d3276f0b7
sandy1999 <sandyrockstar19@gmail.com>
2019-02-07 23:38:34 +0530

    added: sigmoid error function

--- error_functions.py
+++ error_functions.py
@@ -1,5 +1,12 @@
 import numpy as np # for matrix maths 
 
+# sigmoid function for two classes
+def sigmoid(L):
+    for i, number in enumerate(L):
+        L[i] = np.divide(1, 1+np.exp(number))
+    return L
+
+# softmax function for more than one class 
 def softmax(L):
     base_dividend = np.sum(np.exp(L))
     for i, number in enumerate(L):
@@ -7,4 +14,6 @@ def softmax(L):
 
     return L
 
+
+print(sigmoid([0.6, 0.9]))
 print(softmax([1,2,3,4]))
