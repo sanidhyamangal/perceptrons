a9a59aaaeb63ed22222f4883e509c50d7a92c43d
sandy1999 <sandyrockstar19@gmail.com>
2019-02-09 00:56:06 +0530

    added: cross_entropy error function

--- error_functions.py
+++ error_functions.py
@@ -14,6 +14,14 @@ def softmax(L):
 
     return L
 
+# cross-entropy function 
+def cross_entropy(Y, P):
+    Y = np.float_(Y)
+    P = np.float_(P)
 
+    entropy = -np.sum((Y) * np.log(P) + (1- Y) * np.log(1 - P))
+
+    return entropy
 print(sigmoid([0.6, 0.9]))
 print(softmax([1,2,3,4]))
+print(cross_entropy([1,1,0], [0.8, 0.7, 0.1]))
